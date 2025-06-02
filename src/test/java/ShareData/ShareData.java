4ef6e0a9c88dd27b462f39c794de024593a207be
Ioana Antal <ioana.potinteu@gmail.com>
2025-06-02 14:50:59 +0300

    Added browser service implementation

--- src/test/java/ShareData/ShareData.java
+++ src/test/java/ShareData/ShareData.java
@@ -1,5 +1,6 @@
 package ShareData;
 
+import ShareData.browser.BrowserFactory;
 import configFile.ConfigFile;
 import configFile.configNode.ConfigurationNode;
 import org.openqa.selenium.WebDriver;
@@ -15,18 +16,13 @@ public class ShareData {
     private WebDriver driver;
 
     @BeforeMethod
-    public void prepareBrowser(){
+    public void prepareBrowser() {
 
-        ConfigurationNode configurationNode = ConfigFile.createConfigNode(ConfigurationNode.class);
-
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(configurationNode.driverConfigNode.url);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver = new BrowserFactory().getBrowserFactory();
     }
 
     @AfterMethod
-    public void clearBrowser(){
+    public void clearBrowser() {
         driver.quit();
     }
 
---
+++ src/test/java/ShareData/browser/service/BrowserService.java
@@ -0,0 +1,10 @@
+package ShareData.browser.service;
+
+import configFile.configNode.DriverConfigNode;
+
+public interface BrowserService {
+    void openBrowser(DriverConfigNode driverConfigNode);
+   Object getBrowserOptions(DriverConfigNode driverConfigNode);
+
+
+}
