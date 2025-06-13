2259053f1f8aa1064e83f40f8f722e2a1fc2dc0f
Ioana Antal <ioana.potinteu@gmail.com>
2025-06-13 09:06:56 +0300

    Added second pipeline yml

--- src/test/java/ShareData/browser/service/BrowserService.java
+++ src/test/java/ShareData/browser/service/BrowserService.java
@@ -5,6 +5,4 @@ import configFile.configNode.DriverConfigNode;
 public interface BrowserService {
     void openBrowser(DriverConfigNode driverConfigNode);
    Object getBrowserOptions(DriverConfigNode driverConfigNode);
-
-
 }
